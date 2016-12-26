fd52e831b21c2a6277f75cc263013d2e8300c176
gcaaa31928 <gcaaa31928@gmail.com>
2016-12-26 20:23:41 +0800

    added csv controller on magna tag reader

--- utils/magna_tag_reader.py
+++ utils/magna_tag_reader.py
@@ -1,5 +1,17 @@
 import csv
-class MagnaTagReader:
+import os
 
+from definitions import ROOT_DIR
+
+
+class MagnaTagReader:
     def __init__(self):
-        pass
+        self.tag_file = open(os.path.join(ROOT_DIR, 'annotations_final.csv'))
+        self.mp3_dir = './mp3'
+
+    def read(self):
+        for row in csv.reader(self.tag_file, delimiter=','):
+            if row[1] == '1':
+
+
+MagnaTagReader().read()
